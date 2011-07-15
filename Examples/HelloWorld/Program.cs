18c9813d2be3dbf470e0bd2dd82a201092b57545
Markus Johnsson <markus.johnsson@infviz.com>
2011-07-15 15:28:45 +0200

    Added HelloWorld application

---
+++ Examples/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSIL.Dom;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var target = Element.GetById("target");
+            target.AppendChild(new Element("h1") { TextContent = "Hello World" });
+        }
+    }
+}
