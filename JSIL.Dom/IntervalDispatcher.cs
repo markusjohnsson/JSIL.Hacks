0787c0be458735a17f53b328e2bf77d00f52a85b
Markus Johnsson <markus.johnsson@infviz.com>
2011-07-15 21:41:12 +0200

    Added IntervalDispatcher, contains JS bindings for setInterval/clearInterval

---
+++ JSIL.Dom/IntervalDispatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSIL.Meta;
+
+namespace JSIL.Dom
+{
+    public class IntervalDispatcher
+    {
+        private object _handle = null;
+
+        public event EventHandler Tick;
+
+        public long Interval { get; private set; }
+
+        public IntervalDispatcher(long interval)
+        {
+            this.Interval = interval;
+        }
+
+        private void OnNativeEvent()
+        {
+            if (Tick != null)
+            {
+                Tick(this, EventArgs.Empty);
+            }
+        }
+
+        [JSReplacement("$this._handle = setInterval(function () { $this.OnNativeEvent(); }, $this.Interval)")]
+        public void Start()
+        {
+        }
+
+        [JSReplacement("clearInterval($this._handle)")]
+        public void Stop()
+        {
+        }
+    }
+}
