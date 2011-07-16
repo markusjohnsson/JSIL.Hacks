0c25b0dc1ab15c4c5d4970c40539df773323f75c
Markus Johnsson <markus.johnsson@infviz.com>
2011-07-16 18:31:59 +0200

    Added HelloWebGL

---
+++ Examples/HelloWebGL/Matrix4.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWebGL
+{
+    public class Matrix4
+    {
+        public double M11, M12, M13, M14;
+        public double M21, M22, M23, M24;
+        public double M31, M32, M33, M34;
+        public double M41, M42, M43, M44;
+
+        public Matrix4()
+        {
+            M11 = M22 = M33 = M44 = 1.0d;
+        }
+
+        public static Matrix4 Translate(double x, double y, double z)
+        {
+            return new Matrix4()
+            {
+                M41 = x,
+                M42 = y,
+                M43 = z
+            };
+        }
+
+        public Matrix4 Multiply(Matrix4 matrix)
+        {
+            return new Matrix4()
+            {
+                M11 = M11 * matrix.M11 + M12 * matrix.M21 + M13 * matrix.M31 + M14 * matrix.M41,
+                M12 = M11 * matrix.M12 + M12 * matrix.M22 + M13 * matrix.M32 + M14 * matrix.M42,
+                M13 = M11 * matrix.M13 + M12 * matrix.M23 + M13 * matrix.M33 + M14 * matrix.M43,
+                M14 = M11 * matrix.M14 + M12 * matrix.M24 + M13 * matrix.M34 + M14 * matrix.M44,
+
+                M21 = M21 * matrix.M11 + M22 * matrix.M21 + M23 * matrix.M31 + M24 * matrix.M41,
+                M22 = M21 * matrix.M12 + M22 * matrix.M22 + M23 * matrix.M32 + M24 * matrix.M42,
+                M23 = M21 * matrix.M13 + M22 * matrix.M23 + M23 * matrix.M33 + M24 * matrix.M43,
+                M24 = M21 * matrix.M14 + M22 * matrix.M24 + M23 * matrix.M34 + M24 * matrix.M44,
+
+                M31 = M31 * matrix.M11 + M32 * matrix.M21 + M33 * matrix.M31 + M34 * matrix.M41,
+                M32 = M31 * matrix.M12 + M32 * matrix.M22 + M33 * matrix.M32 + M34 * matrix.M42,
+                M33 = M31 * matrix.M13 + M32 * matrix.M23 + M33 * matrix.M33 + M34 * matrix.M43,
+                M34 = M31 * matrix.M14 + M32 * matrix.M24 + M33 * matrix.M34 + M34 * matrix.M44,
+
+                M41 = M41 * matrix.M11 + M42 * matrix.M21 + M43 * matrix.M31 + M44 * matrix.M41,
+                M42 = M41 * matrix.M12 + M42 * matrix.M22 + M43 * matrix.M32 + M44 * matrix.M42,
+                M43 = M41 * matrix.M13 + M42 * matrix.M23 + M43 * matrix.M33 + M44 * matrix.M43,
+                M44 = M41 * matrix.M14 + M42 * matrix.M24 + M43 * matrix.M34 + M44 * matrix.M44
+            };
+        }
+
+
+        public static Matrix4 Perspective(double fovy, double aspect, double near, double far)
+        {
+            var top = near * Math.Tan(fovy * Math.PI / 360.0);
+            var right = top * aspect;
+            return Matrix4.Frustum(-right, right, -top, top, near, far);
+        }
+
+        private static Matrix4 Frustum(
+            double left, double right, double bottom, double top, double near, double far)
+        {
+            var mtx = new Matrix4();
+            var rl = (right - left);
+            var tb = (top - bottom);
+            var fn = (far - near);
+            
+            mtx.M11 = (near * 2) / rl;
+            mtx.M22 = (near * 2) / tb;
+            
+            mtx.M31 = (right + left) / rl;
+            mtx.M32 = (top + bottom) / tb;
+            mtx.M33 = -(far + near) / fn;
+            mtx.M34 = -1;
+
+            mtx.M43 = -(far * near * 2) / fn;
+            mtx.M44 = 0;
+            
+            return mtx;
+        }
+
+        public double[] ToArray()
+        {
+            return new double[] { 
+                M11, M12, M13, M14,
+                M21, M22, M23, M24,
+                M31, M32, M33, M34,
+                M41, M42, M43, M44
+            };
+        }
+    }
+}
---
+++ Examples/HelloWebGL/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSIL.Dom;
+using JSIL.WebGL;
+
+namespace HelloWebGL
+{
+    class PositionBuffer
+    {
+        public GLBuffer Buffer { get; set; }
+        public int ItemSize { get; set; }
+        public int NumItems { get; set; }
+    }
+
+    public class Program
+    {
+        private static WebGLContext gl;
+        private static PositionBuffer triangleVertexPositionBuffer;
+        private static PositionBuffer squareVertexPositionBuffer;
+        private static double viewportWidth;
+        private static double viewportHeight;
+        private static GLShaderProgram shaderProgram;
+        private static GLAttributeLocation vertexPositionAttribute;
+        private static GLUniformLocation mvMatrixUniform;
+        private static GLUniformLocation pMatrixUniform;
+        private static Matrix4 pMatrix;
+        private static Matrix4 mvMatrix;
+
+        public static void Main()
+        {
+            var target = Element.GetById("target");
+            var canvas = new Canvas();
+            target.AppendChild(canvas);
+
+            initGL(canvas);
+            initShaders();
+            initBuffers();
+
+            gl.ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl.Enable(gl.DepthTest);
+
+            drawScene();
+        }
+
+        private static void drawScene()
+        {
+            gl.Viewport(0, 0, viewportWidth, viewportHeight);
+            gl.Clear(gl.ColorBufferBit | gl.DepthBufferBit);
+            pMatrix = Matrix4.Perspective(45, viewportWidth / viewportHeight, 0.1, 100.0);
+
+            // Draw triangle
+            mvMatrix = Matrix4.Translate(-1.5, 0.0, -7.0);
+            gl.BindBuffer(gl.ArrayBuffer, triangleVertexPositionBuffer.Buffer);
+            gl.VertexAttributePointer(vertexPositionAttribute, triangleVertexPositionBuffer.ItemSize, gl.Float, false, 0, 0);
+            setMatrixUniforms();
+            gl.DrawArrays(gl.Triangles, 0, triangleVertexPositionBuffer.NumItems);
+
+            // Draw square
+            mvMatrix = Matrix4.Translate(1.5, 0.0, -7.0);
+            gl.BindBuffer(gl.ArrayBuffer, squareVertexPositionBuffer.Buffer);
+            gl.VertexAttributePointer(vertexPositionAttribute, squareVertexPositionBuffer.ItemSize, gl.Float, false, 0, 0);
+            setMatrixUniforms();
+            gl.DrawArrays(gl.TriangleStrip, 0, squareVertexPositionBuffer.NumItems);
+        }
+
+        private static void setMatrixUniforms()
+        {
+            gl.UniformMatrix4fv(pMatrixUniform, false, pMatrix.ToArray());
+            gl.UniformMatrix4fv(mvMatrixUniform, false, mvMatrix.ToArray());
+        }
+
+        private static void initBuffers()
+        {
+            triangleVertexPositionBuffer = new PositionBuffer();
+            triangleVertexPositionBuffer.Buffer = gl.CreateBuffer();
+            gl.BindBuffer(gl.ArrayBuffer, triangleVertexPositionBuffer.Buffer);
+            var vertices = new [] {
+                 0.0,  1.0,  0.0,
+                -1.0, -1.0,  0.0,
+                 1.0, -1.0,  0.0
+            };
+            gl.BufferData(gl.ArrayBuffer, Float32Array.Create(vertices), gl.StaticDraw);
+            triangleVertexPositionBuffer.ItemSize = 3;
+            triangleVertexPositionBuffer.NumItems = 3;
+
+            squareVertexPositionBuffer = new PositionBuffer();
+            squareVertexPositionBuffer.Buffer = gl.CreateBuffer();
+            gl.BindBuffer(gl.ArrayBuffer, squareVertexPositionBuffer.Buffer);
+            vertices = new [] {
+                 1.0,  1.0,  0.0,
+                -1.0,  1.0,  0.0,
+                 1.0, -1.0,  0.0,
+                -1.0, -1.0,  0.0
+            };
+            gl.BufferData(gl.ArrayBuffer, Float32Array.Create(vertices), gl.StaticDraw);
+            squareVertexPositionBuffer.ItemSize = 3;
+            squareVertexPositionBuffer.NumItems = 4;
+        }
+
+        private static void initShaders()
+        {
+            var fragmentShader = GetShader(gl, "shader-fs");
+            var vertexShader = GetShader(gl, "shader-vs");
+
+            shaderProgram = gl.CreateProgram();
+            gl.AttachShader(shaderProgram, vertexShader);
+            gl.AttachShader(shaderProgram, fragmentShader);
+            gl.LinkProgram(shaderProgram);
+
+            if (!gl.GetProgramParameter(shaderProgram, gl.LinkStatus))
+            {
+                throw new Exception("Could not initialize shaders");
+            }
+
+            gl.UseProgram(shaderProgram);
+
+            vertexPositionAttribute = gl.GetAttributeLocation(shaderProgram, "aVertexPosition");
+            gl.EnableVertexAttributeArray(vertexPositionAttribute);
+
+            pMatrixUniform = gl.GetUniformLocation(shaderProgram, "uPMatrix");
+            mvMatrixUniform = gl.GetUniformLocation(shaderProgram, "uMVMatrix");
+        }
+
+        private static GLShader GetShader(WebGLContext gl, string id)
+        {
+            var shaderScript = Element.GetById(id);
+
+            var str = shaderScript.TextContent;
+            if (str == "" || str == null)
+            {
+                var k = shaderScript.FirstChild;
+                while (k != null)
+                {
+                    if (k.NodeType == 3)
+                        str += k.TextContent;
+                    k = k.NextSibling;
+                }
+            }
+
+            GLShader shader;
+            if (shaderScript.GetAttributeValue("type") == "x-shader/x-fragment")
+            {
+                shader = gl.CreateShader(gl.FragmentShader);
+            }
+            else if (shaderScript.GetAttributeValue("type") == "x-shader/x-vertex")
+            {
+                shader = gl.CreateShader(gl.VertexShader);
+            }
+            else
+            {
+                return null;
+            }
+
+            gl.ShaderSource(shader, str);
+            gl.CompileShader(shader);
+
+            if (!gl.GetShaderParameter(shader, gl.CompileStatus))
+            {
+                throw new Exception("Shader compilation failed");
+            }
+
+            return shader;
+        }
+
+        private static void initGL(Canvas canvas)
+        {
+            gl = canvas.GetWebGLContext();
+            viewportWidth = canvas.Width = 500;
+            viewportHeight = canvas.Height = 500;
+        }
+    }
+}
